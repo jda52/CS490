40fe09268cbc0167294b95ea70d981bb297643f7
JoshA2000 <56370202+JoshA2000@users.noreply.github.com>
2022-02-03 11:25:50 -0500

    Able to hash

--- loginRetrival.py
+++ loginRetrival.py
@@ -1,14 +1,32 @@
 import mysql.connector
+import os
+import bcrypt
+
+def hashPassword(password):
+    hashedPassword = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashedPassword
+
+def checkPassword(results):
+    for entry in results:
+        hash = entry[1].encode()
+        if bcrypt.checkpw(password.encode(), hash):
+            return True
+    return False
 
 try:
     logincnx = mysql.connector.connect(host = "sql1.njit.edu", user= "jda52", password = "3Tdb$h90+&", database = "jda52")
     mycursor = logincnx.cursor()
-    mycursor.execute("SHOW COLUMNS FROM Login")
-    result = mycursor.fetchall()
 
-    for field in result:
-        print(field)
+    Username = "Jas672"
+    password = 'N3*021F^*'
 
+    query = "SELECT * FROM Login"
+    mycursor.execute(query)
+
+    results = mycursor.fetchall()
+    print(checkPassword(results))
+    
+        
 except mysql.connector.Error as err:
     print(err)
 finally:
