3ad8d43689aa074c6c00a5e76342f716e0e8cbc0
Joshua D Amora <jda52@afsconnect1.njit.edu>
2022-02-20 21:14:07 -0500

    Slight adjustments

--- addQuestion.php
+++ addQuestion.php
@@ -1,6 +1,9 @@
 <?php
     include "dbcnx.php";
 
+    $str_json = file_get_contents("php://input"); 
+    $response = json_decode($str_json, true);
+
     $type = "";
     $quest = "";
     $input = "";
@@ -8,11 +11,15 @@
     $diff = "";
     $questID = "";
 
-    $query = "INSERT INTO Question Bank (Type, Question, Input, Output, Difficulty, ID) VALUES";
+    $query = "INSERT INTO Question_Bank (Topic, Question, Input, Output, Difficulty, ID) VALUES ($type, $quest, $input, $output, $diff, $questID)";
     $add = $mycnx->query($query);
     if (!$add)
     {
         echo "Error " . $query . "<br>" . $conn->error;
+    }
+    else
+    {
+        echo 'Question added';
     }
     
 ?>
---
+++ listQuestions.php
@@ -0,0 +1,17 @@
+<?php
+    include "dbcnx.php";
+
+    $str_json = file_get_contents("php://input"); 
+    $response = json_decode($str_json, true);
+
+    $com = "SELECT * FROM Question_Bank";
+    $result = $mycnx->query($com);
+
+    $arr = [];
+    while($row = $result->fetch_assoc())
+    {
+        $arr[] = array('quest'=> $row['Question']);
+    }
+    $json_res = json_encode($response);
+    echo $json_res;
+?>
