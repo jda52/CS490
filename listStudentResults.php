5755effebf03706cd752cd6f00836f0169c844e1
Joshua D Amora <jda52@afsaccess1.njit.edu>
2022-03-09 19:14:52 -0500

    Final additions

--- listStudentResults.php
+++ listStudentResults.php
@@ -1,25 +1,26 @@
 <?php
     include "dbcnx.php";
 
-    #if(isset($response['Username'])) $user = $response['Username'];
-    #if(isset($response['ExamID'])) $exam = $response['ExamID'];
-    $user = 'student';
-    $exam = 2;
-    #$query = "SELECT * FROM ((Student_Results
-    #          INNER JOIN Exam_Questions_Map ON Student_Results.ExamID = Exam_Questions_Map.ExamID) 
-     #         INNER JOIN Question_Bank ON Student_Results.QID = Question_Bank.QID)";
+    $str_json = file_get_contents("php://input"); 
+    $response = json_decode($str_json, true);
+    
+    if(isset($response['student_id'])) $user = $response['student_id'];
+    if(isset($response['exam_id'])) $exam = intval($response['exam_id']);
+
      $query = "SELECT * FROM ((Student_Results
               INNER JOIN Exam_Questions_Map ON Student_Results.ExamID = Exam_Questions_Map.ExamID AND Student_Results.QID = Exam_Questions_Map.QID) 
               INNER JOIN Question_Bank ON Student_Results.QID = Question_Bank.QID)";
-    #$query2 = 'SELECT * FROM Exam_Question_Map WHERE ExamID = $exam';
+
     $result = $mycnx->query($query);
     $arr = [];
+
     while($row = $result->fetch_assoc())
     {
         if ($row['Username'] == $user AND  $row['ExamID'] == $exam)
         {   
-          $arr[] =  array('question_id' => $row['QID'], 'question' => $row['Question'], 'student_answer' => $row['Answer'], 'testcases' => $row['TestCase'], 'points' => $row['Points']);
+          $arr[] =  array('question_id' => $row['QID'], 'question' => $row['Question'], 'student_answer' => $row['Answer'], 'Func_Score'  => $row['Function_Score'], 'TC1' => $row['TC1_Score'], 'TC2' => $row['TC2_Score'], 'testcases' => $row['TestCase'], 'points' => $row['Points'], 'comments'=> $row['Comments']);
         }
     }
     echo json_encode($arr);
+    $mycnx->close();
 ?>
--- submission.php
+++ submission.php
@@ -4,26 +4,27 @@
     $str_json = file_get_contents("php://input"); 
     $response = json_decode($str_json, true);
 
-    if(isset($response['Username'])) $user = "'".$response['Username']."'";
-    if(isset($response['Answers'])) $answers = $response['Answers'];
+    if(isset($response['user_id'])) $user = "'".$response['user_id']."'";
+    if(isset($response['exam_id'])) $exam = $response['exam_id'];
     if(isset($response['questions'])) $questions = $response['questions'];
-    if(isset($response['ExamID'])) $exam = $response['ExamID'];
-    if(isset($response['examName'])) $examName = "'".$response['examName']."'";
+    
     
     $indices = count($questions);
-    
-    $query1 = "INSERT INTO Student_Exams (Username, ExamID, Exam_Name) VALUES ($user, $exam, $examName)";
-    $add = $mycnx->query($query1);
+    #$query1 = "INSERT INTO Student_Exams (Username, ExamID, Exam_Name) VALUES ($user, $exam, $examName)";
+    #$add = $mycnx->query($query1);
     $status = true;
     for ($x = 0; $x < $indices; $x++)
     {
+        
         $iteration = $questions[$x];
-        $QID = $iteration['qid'];
-        $answer = $iteration['answer'];
-        $query = "INSERT INTO Student_Results (Username, ExamID, QID, Answer) VALUES ($user, $exID, $QID, $answer)";
+        $QID = $iteration['question_id'];
+        $answer = "'".$iteration['answer']."'";
+        #query3 = "INSERT INTO Exam_Questions_Map (ExamID, QID, Points) VALUES ($row[0], $qid, $points)";
+        $query = "INSERT INTO Student_Results (Username, ExamID, QID, Answer) VALUES ($user, $exam, $QID, $answer)";
         $add = $mycnx->query($query);
         if (!$add)
         {
+            $status = !$status;
             break;
         }
     }
@@ -36,4 +37,5 @@
       $response = array('response' => 'Exam submitted successfully');
     }
     echo json_encode($response);
+    $mycnx->close();
 ?>
